e45ddb1000052ad55cf3b23323d4f1caf9aeeefe
nuburooj <nuburoojkhattak@gmail.com>
2024-02-01 19:54:34 -0500

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
+# squared_minus_one = list()
+
+# for n in range(1,11):
+#     squared_minus_one.append((n ** 2)- 1)
+
+# print(squared_minus_one)
+
+# squared_minus_one = [(n ** 2) - 1 for n in range(1, 11)]
+# print(squared_minus_one)
+
 def return_evens(num_list):
-    pass
+    even_numbers = [n for n in num_list if n % 2 == 0]
+    return even_numbers
+
+    # for n in num_list:
+    #     if n % 2 == 0:
+    #         return return_evens.append(n)
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_sentence = [s + "!" for s in sentence_list]
+    return exclaimed_sentence
+
